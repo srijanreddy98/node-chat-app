7d04adfd53c6070831ad100b85cfbd8b10ebb6ef
srijanreddy98 <srijanreddy98@gmail.com>
2017-12-16 20:53:43 +0530

    0.0.1v2

--- server/server.js
+++ server/server.js
@@ -1,9 +1,15 @@
 const path = require('path');
 const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 const port = process.env.PORT || 3000;
 app.use(express.static(publicPath));
-
-app.listen(port, () => console.log(`Server is up on port:${port}`));
+ io.on('connection', (socket) => {
+     console.log('New user connected');
+ });
+server.listen(port, () => console.log(`Server is up on port:${port}`));
